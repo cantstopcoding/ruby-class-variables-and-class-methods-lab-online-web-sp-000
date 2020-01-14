9d10eae019b357fb1dc785eac7bd5e6146fb20e4
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-14 18:44:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,6 +34,6 @@ class Song
     end
     
     def self.artist_count
-      Hash[*@@artists.group_by{ |v| v }.flat_map{ |k, v| [k, v.size] }]
+      Hash[@@artists.group_by{ |v| v }.flat_map{ |k, v| [k, v.size] }]
     end 
 end 
